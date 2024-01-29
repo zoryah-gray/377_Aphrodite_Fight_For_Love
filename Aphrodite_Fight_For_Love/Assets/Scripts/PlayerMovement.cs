4262edef021e128d88724f14372d7c9d794fc329
Archie Silverstein <133781221+SpencerArchSilverstein@users.noreply.github.com>
2024-01-28 21:37:09 -0600

    Player movement

--- Aphrodite_Fight_For_Love/Assets/Scripts/PlayerMovement.cs
+++ Aphrodite_Fight_For_Love/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace AphroditeFightCode
 {
@@ -8,21 +9,37 @@ namespace AphroditeFightCode
     {
         // Start is called before the first frame update
         private PlayerInputs input = null;
+        private Vector2 moveVector = Vector2.zero;
+        private Rigidbody2D rb = null;
+        private float moveSpeed = 10f;
         private void Awake()
         {
             input = new PlayerInputs();
+            rb = GetComponent<Rigidbody2D>();
         }
         private void OnEnable()
         {
             input.Enable();
+            input.Player.Movement.performed += OnMovementPerformed;
+            input.Player.Movement.canceled += OnMovementCancelled;
         }
         private void OnDisable()
         {
             input.Disable();
+            input.Player.Movement.performed -= OnMovementPerformed;
+            input.Player.Movement.canceled -= OnMovementCancelled;
         }
-        private void OnAnimatorMove()
+        private void FixedUpdate()
         {
-            
+            rb.velocity = moveVector * moveSpeed;
+        }
+        private void OnMovementPerformed(InputAction.CallbackContext val)
+        {
+            moveVector = val.ReadValue<Vector2>();
+        }
+        private void OnMovementCancelled(InputAction.CallbackContext val)
+        {
+            moveVector = Vector2.zero;
         }
     }
 }
