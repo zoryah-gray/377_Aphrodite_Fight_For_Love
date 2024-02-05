4b991dd01bac1f7f437c4db7509611719a256087
zoryah-gray <zoryahgray2025@u.northwestern.edu>
2024-02-04 21:31:39 -0600

    deleted scene due to merge conflicts

--- Aphrodite_Fight_For_Love/Assets/Puzzle/LightKeypad/KeyPadContoller.cs
+++ Aphrodite_Fight_For_Love/Assets/Puzzle/LightKeypad/KeyPadContoller.cs
@@ -12,14 +12,26 @@ namespace AphroditeFightCode
     {
         [Header("Button Objects")]
         [SerializeField] private GameObject currentButton;
+<<<<<<< Updated upstream
         public GameObject firstButton;
         public Button[] buttons;
+=======
+  
+        public Button firstButton;
+     
+        private Color originalBtnColor = Color.white;
+        private Color flashColor = new Color(1f, 0.97f, 0.6f, 1f); //#FFF899
+        
+        public List<Button> keypadButtons;
+        
+>>>>>>> Stashed changes
         [Header("Code Tracker")]
 //        [SerializeField] private int idx = 0;
         public int codeID = 0;
         //[SerializeField] private int[] code;
         [SerializeField] private List<int> code;
         [SerializeField] public Queue<int> codeQ = new Queue<int>();
+<<<<<<< Updated upstream
         public List<int> currentInput;
         [Header("Action Map")]
         [SerializeField] private ModifiedActionMap actionMapI;
@@ -44,12 +56,52 @@ namespace AphroditeFightCode
             EventSystem.current.SetSelectedGameObject(firstButton);
             currentButton = firstButton;
             Debug.Log(InputSystem.FindControls("UI"));
+=======
+  
+        
+     
+        public List<int> currentInput;
+        [Header("Puzzle Settings")]
+        public float flashDuration = 0.25f;
+        public float delayBtFlashes = 0.42f;
+        
+        [Header("Action Map")]
+        [SerializeField] private ModifiedActionMap actionMap;
+
+        private void OnEnable()
+        {
+            if (actionMap == null)
+            {
+                actionMap = new ModifiedActionMap();
+            }
+  
+            currentButton = EventSystem.current.currentSelectedGameObject;
+
+          
+     
+            actionMap.Player.Disable();
+            actionMap.UI.Enable();
+            EventSystem.current.SetSelectedGameObject(null);
+            Initalize();
+
+            //EventSystem.current.SetSelectedGameObject(firstButton);
+            //currentButton = firstButton;
+        
+>>>>>>> Stashed changes
         }
 
         private void OnDisable()
         {
+<<<<<<< Updated upstream
             actionMap.Player.Enable();
             actionMap.UI.Disable();
+=======
+     
+            actionMap.Player.Enable();
+            actionMap.UI.Disable();
+            StopAllCoroutines();
+        
+>>>>>>> Stashed changes
         }
 
 
@@ -82,6 +134,15 @@ namespace AphroditeFightCode
             currentInput.Add(keyID);
         }
 
+<<<<<<< Updated upstream
+=======
+  
+        private void StartSequenceHint(float duration)
+        {
+            return;
+        }
+     
+>>>>>>> Stashed changes
         public void SubmitPattern()
         {
             // evaluate if the pattern and code are the same
@@ -92,9 +153,46 @@ namespace AphroditeFightCode
             currentInput.Clear();
         }
 
+<<<<<<< Updated upstream
         private void StartSequenceHint(float duration)
         {
             return;
+=======
+        private void FlashPuzzleAnswer()
+        
+        {
+            
+
+            foreach (var i in code)
+            {
+                Debug.Log(i + " | " + keypadButtons[i - 1].gameObject.name);
+                Image btnImg = keypadButtons[i - 1].GetComponent<Image>();
+
+
+                LeanTween.value(btnImg.gameObject, 1f, 0.35f, flashDuration)
+                    .setEase(LeanTweenType.easeInOutQuad)
+                    .setLoopPingPong(1)
+                    .setDelay(i * (flashDuration + delayBtFlashes))
+                    .setOnUpdate((float alpha) =>
+                    {
+                        // Update button color alpha
+                        Color currentColor = btnImg.color;
+                        currentColor.a = alpha;
+                        btnImg.color = currentColor;
+                    })
+                    .setOnComplete(() =>
+                    {
+                        if (i == code[code.Count - 1])
+                        {
+                            EventSystem.current.SetSelectedGameObject(firstButton.gameObject);
+                            currentButton = firstButton.gameObject;
+                        }
+
+                        
+                    });
+            }
+            
+>>>>>>> Stashed changes
         }
 
         private void ResetCode()
@@ -104,10 +202,20 @@ namespace AphroditeFightCode
 
         public void OnReturn()
         {
+<<<<<<< Updated upstream
+=======
+  
+     
+>>>>>>> Stashed changes
             actionMap.Player.Enable();
             actionMap.UI.Disable();
             GameData.freezePlayer = false;
             GameData.inKeypadPuzzle = false;
+<<<<<<< Updated upstream
+=======
+            StopAllCoroutines();
+        
+>>>>>>> Stashed changes
             gameObject.SetActive(false);
         }
 
