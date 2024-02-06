fb83d4cb5814527f726ad93caa3a76cf014a70eb
arojas1234 <106895525+arojas1234@users.noreply.github.com>
2024-02-06 16:16:10 -0600

    enemy vs player layers working!? fixed spacebar attack

--- Aphrodite_Fight_For_Love/Assets/Scripts/PlayerAttack.cs
+++ Aphrodite_Fight_For_Love/Assets/Scripts/PlayerAttack.cs
@@ -8,7 +8,13 @@ namespace AphroditeFightCode
     {
         public PlayerMovement playerMovement;
         public Transform meleeBox;
-        void Start ()
+        public Transform attackBox;
+        public LayerMask enemyLayers;
+        public Vector3 curPosition;
+
+        Vector2 rectangleSize = new Vector2(0.9f, 0.45f);
+
+        void Start()
         {
             meleeBox = transform.Find("MeleeBox");
         }
@@ -17,8 +23,8 @@ namespace AphroditeFightCode
         {
             if (playerMovement.directionInt == 1) //Up
             {
-                meleeBox.position = new Vector3(transform.position.x, transform.position.y + 0.8f,transform.position.z);
-                Debug.Log(playerMovement.animAfterLeft);
+                meleeBox.position = new Vector3(transform.position.x, transform.position.y + 0.8f, transform.position.z);
+        //        Debug.Log(playerMovement.animAfterLeft);
                 if (playerMovement.animAfterLeft == true)
                 {
                     meleeBox.rotation = Quaternion.Euler(0f, 0f, 270f);
@@ -36,7 +42,7 @@ namespace AphroditeFightCode
             }
             else if (playerMovement.directionInt == 3) //Down
             {
-                Debug.Log(playerMovement.animAfterLeft);
+        //        Debug.Log(playerMovement.animAfterLeft);
 
                 meleeBox.position = new Vector3(transform.position.x, transform.position.y - 0.8f, transform.position.z);
                 if (playerMovement.animAfterLeft == true)
@@ -53,8 +59,35 @@ namespace AphroditeFightCode
             {
                 meleeBox.position = new Vector3(transform.position.x - 0.8f, transform.position.y, transform.position.z);
                 meleeBox.rotation = Quaternion.Euler(0f, 0f, 0f);
-
             }
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                curPosition = meleeBox.position;
+                Attack();
+                Debug.Log("Attack!");
+            }
+
+        }
+        void Attack()
+        {
+            // do correct animation
+            //    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(meleeBox.position, 4.0f, enemyLayers);
+
+            Debug.Log(meleeBox.position + "cur pos" + enemyLayers.ToString());
+
+            Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackBox.position, rectangleSize, 0f, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("We hit an enemy!" + enemy.name);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            //    Gizmos.DrawWireCube(attackBox.position, new Vector2(1.1f, 0.6f));
+            Gizmos.DrawWireSphere(meleeBox.position, 0.5f);
+            //    Gizmos.DrawCube(curPosition, new Vector2(2f, 2f));
 
         }
     }
