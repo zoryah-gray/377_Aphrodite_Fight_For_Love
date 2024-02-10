55846e6a8e60833c940d2abf95d51e2bc15fdb8b
zoryah-gray <zoryahgray2025@u.northwestern.edu>
2024-02-09 21:49:03 -0600

    fixed hopefully

--- Aphrodite_Fight_For_Love/Assets/Puzzle/LightKeypad/Scripts/PlayerKeypadPuzzleController.cs
+++ Aphrodite_Fight_For_Love/Assets/Puzzle/LightKeypad/Scripts/PlayerKeypadPuzzleController.cs
@@ -15,6 +15,7 @@ namespace AphroditeFightCode
         public bool inPuzzleTrigger = false;
         public bool inKeypadPuzzle = false;
         public GameObject keypadUI;
+        public bool inQuestTrigger = false;
 
 
         private void Awake()
@@ -59,6 +60,10 @@ namespace AphroditeFightCode
                 keypadUI.SetActive(true);
                 Debug.Log("Puzzle Activated| in puzzle? " + inKeypadPuzzle + "freeze player>?: " + freezePlayer + " Game data saved: " + GameData.freezePlayer);
 
+            }
+            if (inQuestTrigger)
+            {
+                GameData.startingQuestActions = true;
             }
         }
     }
