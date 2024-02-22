c8d6f67c61ace78d0a82fa85f3eed14f1d5765bc
Archie Silverstein <133781221+SpencerArchSilverstein@users.noreply.github.com>
2024-02-21 18:05:01 -0600

    Please say 3 hours of work was for nothing

--- Aphrodite_Fight_For_Love/Assets/Scripts/Hestia.cs
+++ Aphrodite_Fight_For_Love/Assets/Scripts/Hestia.cs
@@ -6,52 +6,72 @@ namespace AphroditeFightCode
 {
     public class Hestia : MonoBehaviour
     {
-        public GameObject fireballPrefab;
+        //public GameObject fireballPrefab;
 
-        public bool canFF;
-        public GameObject FFPrefab;
+        //public bool canFF;
+        //public GameObject FFPrefab;
 
-        public FFState currentState = FFState.ff1;
-        public Sprite ff_empty;
-        public Sprite ff1_Sprite;
-        public Sprite ff2_Sprite;
-        public Sprite ffdamage_Sprite;
-        private SpriteRenderer fftileSpriteRenderer;
-        private float ffloopEnd;
-        private float ffloopInterval = 10f;
+        //public FFState currentState = FFState.ff1;
+        //public Sprite ff_empty;
+        //public Sprite ff1_Sprite;
+        //public Sprite ff2_Sprite;
+        //public Sprite ffdamage_Sprite;
+        //private SpriteRenderer fftileSpriteRenderer;
+        //private float ffloopEnd;
+        //private float ffloopInterval = 10f;
 
-        public GameObject firetile;
+        //public GameObject firetile;
 
-        public float fireballSpeed = 1f;
+        //public float fireballSpeed = 1f;
 
         public Animator hestAnim;
 
         private float loopEnd;
-        public float loopInterval = 20f;
+        public float loopInterval = 4f;
 
         private float holdAnimLoopEnd;
-        private float holdAnimLoopInterval = 20f;
+        private float holdAnimLoopInterval = 5f;
 
         private GameObject player;
         //private SpriteRenderer hestRend;
 
-        private bool HestiaIsLeft;
-        private bool currPullingWall;
+        //private bool HestiaIsLeft;
 
-        public float hasFF = 0f;
+        public GameObject meteorPrefab;
+
+        public GameObject arr1Prefab;
+        public GameObject arr2Prefab;
+        public GameObject arr3Prefab;
+
+        private SpriteRenderer arr1SR;
+        private SpriteRenderer arr2SR;
+        private SpriteRenderer arr3SR;
+
+        private Transform arr1T;
+        private Transform arr2T;
+        private Transform arr3T;
+
+        public float meteorSpeed;
+
+
+
+
+        //private bool currPullingWall;
+
+        //public float hasFF = 0f;
 
         //private GameObject fftile;
 
-        public GameObject leftWall;
-        public GameObject rightWall;
+        //public GameObject leftWall;
+        //public GameObject rightWall;
 
-        public enum FFState
-        {
-            empty,
-            ff1,
-            ff2,
-            ffdamage
-        }
+        //public enum FFState
+        //{
+        //    empty,
+        //    ff1,
+        //    ff2,
+        //    ffdamage
+        //}
 
         // Start is called before the first frame update
         void Start()
@@ -60,11 +80,21 @@ namespace AphroditeFightCode
             loopEnd = -loopInterval;
             Animator hestAnim = GetComponent<Animator>();
             hestAnim.SetInteger("Hold", 1);
-            HestiaIsLeft = true;
-            currPullingWall = false;
-            ffloopEnd = -ffloopInterval;
-            canFF = false;
-            fftileSpriteRenderer = firetile.GetComponent<SpriteRenderer>();
+
+            arr1SR = arr1Prefab.GetComponent<SpriteRenderer>();
+            arr2SR = arr2Prefab.GetComponent<SpriteRenderer>();
+            arr3SR = arr3Prefab.GetComponent<SpriteRenderer>();
+
+            arr1T = arr1Prefab.GetComponent<Transform>();
+            arr2T = arr2Prefab.GetComponent<Transform>();
+            arr3T = arr3Prefab.GetComponent<Transform>();
+            meteorSpeed = 3f;
+
+            //HestiaIsLeft = true;
+            //currPullingWall = false;
+            //ffloopEnd = -ffloopInterval;
+            //canFF = false;
+            //fftileSpriteRenderer = firetile.GetComponent<SpriteRenderer>();
         }
 
 
@@ -75,130 +105,257 @@ namespace AphroditeFightCode
             changeHestiaDir();
             HestiaAttackAnim();
             //FFForUpdate();
-
-
-
         }
-        void ShootFireball()
+
+
+        void shootMeteor()
         {
-            var fireballInitPos1 = new Vector3(0f, 0f, 0f);
+            //int dirM = Random.Range(1, 5);
+            int dirM = 4;
+            
+            //from bottom
+            if (dirM == 1)
+            {
+                arr1T.transform.position = new Vector3(-5.45f, -4.5f, 1);
+                arr1T.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                arr2T.transform.position = new Vector3(0f, -4.5f, 1);
+                arr2T.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                arr3T.transform.position = new Vector3(5.45f, -4.5f, 1);
+                arr3T.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
 
-            if (HestiaIsLeft) { fireballInitPos1 = new Vector3(1.5f, 0f, 0f); }
-            if (!HestiaIsLeft) { fireballInitPos1 = new Vector3(-1.5f, 0f, 0f); }
+                showArrow();
+                
+                for (float i = 1; i < 5f; i++)
+                {
+                    GameObject bottomMeteor = Instantiate(meteorPrefab, new Vector3(Random.Range(-8f,-4f) + (i * 4f) - 4f, -10f - i, 0f), Quaternion.identity);
+                    Rigidbody2D bottomMeteorRB = bottomMeteor.GetComponent<Rigidbody2D>();
+                    bottomMeteorRB.velocity = new Vector3(0f, meteorSpeed, 0f);
+                }
+                Invoke("hideArrow", 3f);
+                
+            }
+            
 
-            GameObject fireballGO = Instantiate(fireballPrefab, fireballInitPos1, Quaternion.identity);
-            Vector2 direction = player.transform.position;
-            Rigidbody2D fireballRB = fireballGO.GetComponent<Rigidbody2D>();
-            fireballRB.velocity = direction * fireballSpeed;
+            //from top
+            if (dirM == 2)
+            {
+                arr1T.transform.position = new Vector3(-5.45f, 4.5f, 1);
+                arr1T.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+                arr2T.transform.position = new Vector3(0f, 4.5f, 1);
+                arr2T.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+                arr3T.transform.position = new Vector3(5.45f, 4.5f, 1);
+                arr3T.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+
+                showArrow();
+                for (float i = 1; i < 5f; i++)
+                {
+                    GameObject topMeteor = Instantiate(meteorPrefab, new Vector3(Random.Range(-8f, -4f) + (i*4f) - 4f, 10f + i, 0f), Quaternion.identity);
+                    Rigidbody2D topMeteorRB = topMeteor.GetComponent<Rigidbody2D>();
+                    topMeteor.transform.Rotate(0f, 0f, 180f);
+                    topMeteorRB.velocity = new Vector3(0f, (-1) * meteorSpeed, 0f);
+                }
+                Invoke("hideArrow", 3f);
+            }
+
+            //from left
+            if (dirM == 3)
+            {
+                arr1T.transform.position = new Vector3(-8.25f, 2.5f, 1);
+                arr1T.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+                arr2T.transform.position = new Vector3(-8.25f, 0f, 1);
+                arr2T.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+                arr3T.transform.position = new Vector3(-8.25f, -2.5f, 1);
+                arr3T.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+                showArrow();
+                for (float i = 1; i < 5f; i++)
+                {
+                    GameObject leftMeteor = Instantiate(meteorPrefab, new Vector3(-14f - i, Random.Range(-4f,4f), 0f), Quaternion.identity);
+                    Rigidbody2D leftMeteorRB = leftMeteor.GetComponent<Rigidbody2D>();
+                    leftMeteor.transform.Rotate(0f, 0f, -90f);
+                    leftMeteorRB.velocity = new Vector3(meteorSpeed, 0f, 0f);
+                }
+                Invoke("hideArrow", 3f);
+
+            }
+
+            //from right
+            if (dirM == 4)
+            {
+                arr1T.transform.position = new Vector3(8.25f, 2.5f, 1);
+                arr1T.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                arr2T.transform.position = new Vector3(8.25f, 0f, 1);
+                arr2T.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                arr3T.transform.position = new Vector3(8.25f, -2.5f, 1);
+                arr3T.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                showArrow();
+                
+                for (float i = 1; i < 5f; i++)
+                {
+                    GameObject rightMeteor = Instantiate(meteorPrefab, new Vector3(14f + i,Random.Range(-4f,4f), 0f), Quaternion.identity);
+                    Rigidbody2D rightMeteorRB = rightMeteor.GetComponent<Rigidbody2D>();
+                    rightMeteor.transform.Rotate(0f, 0f, 90f);
+                    rightMeteorRB.velocity = new Vector3((-1) * meteorSpeed, 0f, 0f);
+                }
+                Invoke("hideArrow", 3f);
+
+            }
         }
+
+        void hideArrow()
+        {
+            arr1SR.enabled = false;
+            arr2SR.enabled = false;
+            arr3SR.enabled = false;
+        }
+
+        void showArrow()
+        {
+            arr1SR.enabled = true;
+            arr2SR.enabled = true;
+            arr3SR.enabled = true;
+        }
+
+        //void decideArrow()
+        //{
+        //    int dirM = 1;
+        //    if(dirM == 1)
+        //    {
+
+        //    }
+        //    if (dirM == 2)
+        //    {
+        //        Debug.Log("showFromUpArrow");
+        //    }
+        //    if (dirM == 3)
+        //    {
+        //        Debug.Log("showFromLeftArrow");
+        //    }
+        //    if (dirM == 4)
+        //    {
+        //        Debug.Log("showFromRightArrow");
+        //    }
+        //}
+
+        //void ShootFireball()
+        //{
+        //    var fireballInitPos1 = new Vector3(0f, 0f, 0f);
+
+        //    if (HestiaIsLeft) { fireballInitPos1 = new Vector3(1.5f, 0f, 0f); }
+        //    if (!HestiaIsLeft) { fireballInitPos1 = new Vector3(-1.5f, 0f, 0f); }
+
+        //    GameObject fireballGO = Instantiate(fireballPrefab, fireballInitPos1, Quaternion.identity);
+        //    Vector2 direction = player.transform.position;
+        //    Rigidbody2D fireballRB = fireballGO.GetComponent<Rigidbody2D>();
+        //    fireballRB.velocity = direction * fireballSpeed;
+        //}
 
 
         void changeHestiaDir()
         {
 
-            if (player.transform.position.x > transform.position.x && currPullingWall == false)
+            if (player.transform.position.x > transform.position.x)
             {
                 transform.rotation = Quaternion.Euler(180f, 0f, 180f);
-                HestiaIsLeft = true;
+                //HestiaIsLeft = true;
             }
-            if (player.transform.position.x < transform.position.x && currPullingWall == false)
+            if (player.transform.position.x < transform.position.x)
             {
                 transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                HestiaIsLeft = false;
+                //HestiaIsLeft = false;
             }
         }
 
-        void fireFloor()
-        {
-            if (hasFF % 20 == 0)
-            {
-                var ffTilePos = new Vector3(-3.5f, -2.5f, 0f);
-                GameObject fftile = Instantiate(FFPrefab, ffTilePos, Quaternion.identity);
-                //FFForUpdate();
-                Destroy(fftile, 10f);
-                hasFF = 1;
-            }
-            hasFF += 1;
-        }
+        //void fireFloor()
+        //{
+        //    if (hasFF % 20 == 0)
+        //    {
+        //        var ffTilePos = new Vector3(-3.5f, -2.5f, 0f);
+        //        GameObject fftile = Instantiate(FFPrefab, ffTilePos, Quaternion.identity);
+        //        //FFForUpdate();
+        //        Destroy(fftile, 10f);
+        //        hasFF = 1;
+        //    }
+        //    hasFF += 1;
+        //}
 
-        void fireWall()
-        {
-            Rigidbody2D leftFireWallRB = leftWall.GetComponent<Rigidbody2D>();
-            Rigidbody2D rightFireWallRB = rightWall.GetComponent<Rigidbody2D>();
+        //void fireWall()
+        //{
+        //    Rigidbody2D leftFireWallRB = leftWall.GetComponent<Rigidbody2D>();
+        //    Rigidbody2D rightFireWallRB = rightWall.GetComponent<Rigidbody2D>();
 
-            if (!HestiaIsLeft)
-            {
-                //If wall not moving, set it to left
-                if (leftFireWallRB.velocity.x == 0f) { leftWall.transform.position = new Vector3(-17f, 0f, 0f); }
+        //    if (!HestiaIsLeft)
+        //    {
+        //        //If wall not moving, set it to left
+        //        if (leftFireWallRB.velocity.x == 0f) { leftWall.transform.position = new Vector3(-17f, 0f, 0f); }
 
-                //If it's on the left side, move it from the left
-                if (leftWall.transform.position.x <= -17f) { leftFireWallRB.velocity = leftWall.transform.right * 1.5f; }
-
-
-                //If the left wall is moving back and it's to the left of it's initial position, freeze the velocity
-                //if (leftFireWallRB.velocity.x < 0f && leftWall.transform.position.x <= -17f) { leftFireWallRB.velocity = new Vector2(0f, 0f); }
-            }
+        //        //If it's on the left side, move it from the left
+        //        if (leftWall.transform.position.x <= -17f) { leftFireWallRB.velocity = leftWall.transform.right * 1.5f; }
 
 
-            if (HestiaIsLeft)
-            {
-                //If wall not moving, set it to right
-                if (rightFireWallRB.velocity.x == 0f) { rightWall.transform.position = new Vector3(17f, 0f, 0f); }
-
-                //If it's on the left side, move it from the left
-                if (rightWall.transform.position.x >= 17f) { rightFireWallRB.velocity = rightWall.transform.right * -1.5f; }
-            }
+        //        //If the left wall is moving back and it's to the left of it's initial position, freeze the velocity
+        //        //if (leftFireWallRB.velocity.x < 0f && leftWall.transform.position.x <= -17f) { leftFireWallRB.velocity = new Vector2(0f, 0f); }
+        //    }
 
 
+        //    if (HestiaIsLeft)
+        //    {
+        //        //If wall not moving, set it to right
+        //        if (rightFireWallRB.velocity.x == 0f) { rightWall.transform.position = new Vector3(17f, 0f, 0f); }
+
+        //        //If it's on the left side, move it from the left
+        //        if (rightWall.transform.position.x >= 17f) { rightFireWallRB.velocity = rightWall.transform.right * -1.5f; }
+        //    }
 
 
-            //If it has passed the cave-in time, then move backwards
-            if (rightWall.transform.position.x < 8f) { rightFireWallRB.velocity = rightWall.transform.right * 1.5f; }
-
-            //If the left wall is moving back and it's to the right of it's initial position, freeze the velocity
-            if (rightFireWallRB.velocity.x > 0f && rightWall.transform.position.x >= 17f)
-            {
-                rightWall.transform.position = new Vector3(17f, 0f, 0f);
-                rightFireWallRB.velocity = new Vector2(0f, 0f);
-            }
-
-            if (leftFireWallRB.velocity.x < 0f && leftWall.transform.position.x <= -17f)
-            {
-                leftWall.transform.position = new Vector3(-17f, 0f, 0f);
-                leftFireWallRB.velocity = new Vector2(0f, 0f);
-            }
-
-            //If it has passed the cave-in time, then move backwards
-            if (leftWall.transform.position.x > -8f) { leftFireWallRB.velocity = leftWall.transform.right * -1.5f; }
 
 
-        }
+        //    //If it has passed the cave-in time, then move backwards
+        //    if (rightWall.transform.position.x < 8f) { rightFireWallRB.velocity = rightWall.transform.right * 1.5f; }
+
+        //    //If the left wall is moving back and it's to the right of it's initial position, freeze the velocity
+        //    if (rightFireWallRB.velocity.x > 0f && rightWall.transform.position.x >= 17f)
+        //    {
+        //        rightWall.transform.position = new Vector3(17f, 0f, 0f);
+        //        rightFireWallRB.velocity = new Vector2(0f, 0f);
+        //    }
+
+        //    if (leftFireWallRB.velocity.x < 0f && leftWall.transform.position.x <= -17f)
+        //    {
+        //        leftWall.transform.position = new Vector3(-17f, 0f, 0f);
+        //        leftFireWallRB.velocity = new Vector2(0f, 0f);
+        //    }
+
+        //    //If it has passed the cave-in time, then move backwards
+        //    if (leftWall.transform.position.x > -8f) { leftFireWallRB.velocity = leftWall.transform.right * -1.5f; }
+
+
+        //}
 
         void HestiaAttackAnim()
         {
             if (Time.time - loopEnd >= loopInterval)
             {
-                int statePicker = 3;
+                int statePicker = 2;
                 //int statePicker = Random.Range(1, 4);
                 //int statePicker = Random.Range(1, 3);
                 Debug.Log("statePicker" + statePicker);
 
-                if (statePicker == 1)
-                {
-                    hestAnim.SetInteger("State", 1);
-                    hestAnim.SetInteger("Hold", 1);
-                }
+                //if (statePicker == 1)
+                //{
+                //    hestAnim.SetInteger("State", 1);
+                //    hestAnim.SetInteger("Hold", 1);
+                //}
                 if (statePicker == 2)
                 {
                     hestAnim.SetInteger("State", 2);
                     hestAnim.SetInteger("Hold", 1);
                 }
-                if (statePicker == 3)
-                {
-                    hestAnim.SetInteger("State", 3);
-                    hestAnim.SetInteger("Hold", 1);
-                    //fireWall();
-                }
+                //if (statePicker == 3)
+                //{
+                //    hestAnim.SetInteger("State", 3);
+                //    hestAnim.SetInteger("Hold", 1);
+                //    //fireWall();
+                //}
                 loopEnd = Time.time;
                 if (Time.time - holdAnimLoopEnd >= holdAnimLoopInterval)
                 {
